05b74d0655a9b861b0e1bd245af7d7d088d175e3
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-02 11:39:03 +0530

    setUP project

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import react from 'react';
+function App() {
+  return <div className="App">APP</div>;
+}
+
+export default App;
---
+++ src/reducers/posts.js
@@ -0,0 +1,3 @@
+export default function posts(state=['anurag tripathi is a good boy']){
+    return state;
+}
