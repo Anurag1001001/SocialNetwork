6c6b8fd07073ca66ccccc287ace9ca8727f18403
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-08 15:41:54 +0530

    Adding Router 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 // PACKAGE IMPORT
 import React from "react";
 import { connect } from "react-redux";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
 
 // FILE IMPORT
 import { fetchPosts } from "../actions/posts";
-import { Home, Navbar } from "./";
+import { Home, Navbar, Page404 } from "./";
 
 // dummy component to understand Routing
 
@@ -31,16 +31,22 @@ class App extends React.Component {
           One more thing i want to tell is that <Route /> component will not accept any props apart from the default ones, so there is an alternative react-router-dom provide is to use render prop instead of component prop (any thing i've used inside<Route /> is the props ) in <Route /> like this ---> <Route path='/' render ={() => {Home}}, render prop will get a callback and we can easily pass props there>.
           
           */}
-          <Route
-            exact
-            path="/"
-            render={(props) => {
-              // Rembember i talked about default props that <Route /> give to us, so by using render (props/function don't know exactly what it is.) those default props are not passed to <Home /> component bu we can pass those default props to the callback and to the <Home /> component.
-              return <Home {...props} posts={posts} />;
-            }}
-          />
-          <Route exact path="/Login" component={Login} />
-          <Route exact path="/SignUp" component={SignUp} />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => {
+                // Rembember i talked about default props that <Route /> give to us, so by using render (props/function don't know exactly what it is.) those default props are not passed to <Home /> component bu we can pass those default props to the callback and to the <Home /> component.
+                return <Home {...props} posts={posts} />;
+              }}
+            />
+            <Route path="/Login" component={Login} />
+            <Route path="/SignUp" component={SignUp} />
+            <Route component={Page404} />
+            {/* So ideally how <Route /> component works is : it ideally matches the path mentioned line by line and if it's matches then it route towards those component, and if more paths matches then it render component accordingly, so what i want to create here is if user entered worng URL then i want to display Page404.js component, one more thing i want my <Route /> component go to only one route whatever matches first, so in that case i'm gonna use switch component that i get from react-router-dom.
+          
+          */}
+          </Switch>
         </div>
       </Router>
     );
