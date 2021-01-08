54ccc1849c8fb3a3568a14a669a0644d7f3ddc1b
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-08 17:13:19 +0530

    Form Handling: Uncontrolled components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.emailInputRef = React.createRef();
+    this.passwordInputRef = React.createRef();
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("emailInputRef", this.emailInputRef);
+    console.log("passwordInputRef", this.passwordInputRef);
+  };
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            ref={this.emailInputRef}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            ref={this.passwordInputRef}
+          />
         </div>
         <div className="field">
-          <button>Log In</button>
+          <button onClick={this.handleFormSubmit}>Log In</button>
         </div>
       </form>
     );
@@ -20,3 +41,6 @@ class Login extends Component {
 }
 
 export default Login;
+
+// There are two ways to handle forms in react, the very first one is by Uncontrolled component and the second way is by using Controlled component.
+// 1. By using UnControlled component :- simply means that where data is not managed by react component itself, the form data where the email field, password field is not managed by react itself but will be managed by DOM. So let's for the understanding purpose make our login Form Uncontrolled Component
