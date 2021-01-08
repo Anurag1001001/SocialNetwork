74d6de3d186401725ebd7d9d8e8dfa2a994ea9e9
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-08 13:49:26 +0530

    react router - 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 // PACKAGE IMPORT
 import React from 'react';
 import {connect} from 'react-redux';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 // FILE IMPORT
 import {fetchPosts} from '../actions/posts';
-import {PostsList} from './';
+import {PostsList, Navbar} from './';
+
+
+// dummy component to understand Routing
+
+const Login = () =><div>login</div>
+const SignUp = () => <div>SignUp</div>
+const Home = () => <div>Home</div>
+
+
 
 class App extends React.Component{
   componentDidMount(){
@@ -15,9 +25,35 @@ class App extends React.Component{
   render(){
     const {posts} = this.props;
     return (
-      <div>
-        <PostsList posts= {posts} />
-      </div>
+      // it's my root component so i should tell Router by wrapping all the stuffs of root component, now Router has to work inside it. 
+      <Router>
+        <div>
+          <Navbar />
+          {/* <PostsList posts= {posts} /> */}
+          
+
+          {/* when i click on the given link react router route me to the given path which i mentioned in the <Route> tag */}
+          <ul>
+            <li>
+              <Link to = 'Login'>login</Link>
+            </li>
+            <li>
+              <Link to = 'SignUp'>SignUp</Link>
+            </li>
+            <li>
+              <Link to = '/'>Home</Link>
+            </li>
+          </ul>
+          {/* The benefit of using <Link> that react-router-dom provide is it doesn't reload the page whereas <a href=''> tag reload the page  */}
+
+          {/* Let say i want Navbar to be common in our application then what i would write <Navbar/> outside Route, to specify a path for routing like below */}
+          <Route exact path ='/' component = {Home}  />
+          <Route exact path ='/Login' component = {Login}  />
+          <Route exact path ='/SignUp' component = {SignUp}  />
+
+
+        </div>
+      </Router>
     );
   }
 }
