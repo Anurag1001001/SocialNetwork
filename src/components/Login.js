6ac9a3b3b3307d716cd57f672581fa1351fd22e1
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-08 17:40:57 +0530

    Form handling: By Controlled components

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,18 +2,38 @@ import React, { Component } from "react";
 
 // There are two ways to handle forms in react, the very first one is by Uncontrolled component and the second way is by using Controlled component.
 // 1. By using UnControlled component :- simply means that where data is not managed by react component itself, the form data where the email field, password field is not managed by react itself but will be managed by DOM. So let's for the understanding purpose make our login Form Uncontrolled Component
+// 2. By using controlled component :- means that data will be managed by react component itself unlike unControlled component(data manage by DOM and then sends to the server), here in Controlled component state thing aa jati jhn phle se he data field me empty string reside krega and onChange of input field, value capture kr k state update kr denge and head towards further process.
+
+// We generally manage data by controlled component, in some case where i want to use animation or give alert or popUp to the input field while user typing the field then we can use uncontrolled component.
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.emailInputRef = React.createRef();
-    this.passwordInputRef = React.createRef();
+    // Uncontrolled Component :--
+
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+
+    // Controlled Component :--
+    this.state = {
+      email: "",
+      password: "",
+    };
   }
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
 
   handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log("emailInputRef", this.emailInputRef);
-    console.log("passwordInputRef", this.passwordInputRef);
+    console.log("this.state(Coming from components/login)", this.state);
   };
   render() {
     return (
@@ -24,7 +44,9 @@ class Login extends Component {
             type="email"
             placeholder="Email"
             required
-            ref={this.emailInputRef}
+            // ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
+            value={this.state.email}
           />
         </div>
         <div className="field">
@@ -32,7 +54,9 @@ class Login extends Component {
             type="password"
             placeholder="Password"
             required
-            ref={this.passwordInputRef}
+            // ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
           />
         </div>
         <div className="field">
