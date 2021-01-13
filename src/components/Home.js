f9a0e9a8897715e50a52903655cd5aee2ee7b601
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-13 16:00:59 +0530

    Displaying FriendList to the User

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ import { PostsList, FriendsList } from "./";
 class Home extends Component {
   render() {
     const { posts, friends, isLoggedin } = this.props;
-    console.log("friends, isLoggedin", friends, isLoggedin);
     return (
       <div className="home">
         <PostsList posts={posts} />
